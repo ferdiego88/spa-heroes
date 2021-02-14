8a08af87c3f1c7aef960977b34764f1ac3d92c95
Fernando Santillan <ferdiego88@gmail.com>
2021-02-14 13:00:24 -0500

    NavBar-Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+
+// Rutas
+import { FeatureRoutingModule} from './app.routes';
 
 
 @NgModule({
@@ -17,7 +21,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FeatureRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
     // { path: 'path/:routeParam', component: MyComponent },
     // { path: 'staticPath', component: ... },
@@ -15,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class FeatureRoutingModule {}

dd1d158f0182c532b95ee7532eeb3417eac5aa23
Fernando Santillan <ferdiego88@gmail.com>
2021-02-14 22:06:13 -0500

    fsv

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../providers/heroes.services';
+import { Heroe } from '../../models/heroes.model';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
-
-  constructor(private _heroesService: HeroesService) {
+  heroes: Heroe[] = [];
+  constructor(private heroesService: HeroesService) {
+    // console.log('constructor');
 
   }
 
   ngOnInit(): void {
+    // console.log('ng OnInit');
+    this.heroes = this.heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
---
+++ src/app/models/heroes.model.ts
@@ -0,0 +1,13 @@
+export class Heroe {
+    constructor(public nombre: string,
+                public biografia: string,
+                public rutaImagen: string,
+                public aparicion: string,
+                public casa: string, ){
+    this.nombre = nombre;
+    this.biografia = biografia;
+    this.rutaImagen = rutaImagen;
+    this.aparicion = aparicion;
+    this.casa = casa;
+    }
+}

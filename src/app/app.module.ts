4fa2205262d16f444b702fc1eb5ddbb12c296b6e
Fernando Santillan <ferdiego88@gmail.com>
2021-02-16 11:39:18 -0500

    fsv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,16 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscarheroeComponent } from './components/buscarheroe/buscarheroe.component';
+import { HeroeTarjetaComponent } from './components/tarjetas/heroe-tarjeta/heroe-tarjeta.component';
+
 
 // Routes
 import { FeatureRoutingModule} from './app.routes';
 
 // Providers
 import { HeroesService } from './providers/heroes.services';
-import { HeroeComponent } from './components/heroe/heroe.component';
-import { BuscarheroeComponent } from './components/buscarheroe/buscarheroe.component';
-import { HeroeTarjetaComponent } from './components/tarjetas/heroe-tarjeta/heroe-tarjeta.component';
 
 
 
@@ -28,7 +29,7 @@ import { HeroeTarjetaComponent } from './components/tarjetas/heroe-tarjeta/heroe
     HeroesComponent,
     HeroeComponent,
     BuscarheroeComponent,
-    HeroeTarjetaComponent
+    HeroeTarjetaComponent,
   ],
   imports: [
     BrowserModule,
